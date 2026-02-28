e85a068f30fcdc0d19edb6872814cdf32810ccb6
Pranav-Singh-Devloper <pranavsinghprimo@gmail.com>
2026-02-28 13:52:05 +0530

    fixed models/rating.go

--- backend/internal/models/rating.go
+++ backend/internal/models/rating.go
@@ -3,13 +3,13 @@
 package models
 
 // UserResult represents a single user's rank in the contest 
-type USerResult struch {
+type UserResult struct {
 	UserID string `json:"user_id"`
 	Rank int `json:"rank`
 }
 
 // ContestEndPayload is the exact JSON structure the backen expects 
-type ContestEndPayload struch {
-	ContestID strign `json:"contest_id"`
+type ContestEndPayload struct {
+	ContestID string `json:"contest_id"`
 	Results []UserResult `json:"resutls"`
 }
